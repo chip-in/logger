997eaf459f72bd24cace1d6f461aa2a283255977
mioka47 <41131770+mioka47@users.noreply.github.com>
2022-06-29 15:55:14 +0900

    ADD typescript declaration file (#3)
    * [[PRERELEASE]] ADD typescript declaration file
    
    * [automate_versioning_prerelease] Prerelease 0.1.2-rc179e6a03.0
    
    * [[PRERELEASE]] Update typescript declaration file
    
    * [automate_versioning_prerelease] Prerelease 0.1.2-rc179e6a03.1
    
    Co-authored-by: procube-builder <admin@procube.jp>

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+import { ResourceNode } from "@chip-in/resource-node";
+
+export class Logger {
+  static getLogger(fqdn: string): Log;
+  /**
+   * @param logLevel Set logLevel numerically or by name. The name that can be specified are 'critical', 'error', 'warn', 'log', 'info', 'debug' and 'trace'. The number can be specified from 1 to 7, and the values are in the order in which the names are written. e.g. 3 means 'warn'. The default is 3.
+   */
+  static setLogLevel(logLevel: number | LogLevel): void;
+  static setMaxStringLength(maxStringLength: number): void;
+  static attachUploader(resourceNode: ResourceNode): void;
+}
+export class Log {
+  critical(code: number, message: string, inserts?: Array<string>, numInserts?: Array<number>, timeInserts?: Array<string>, language?: string): void;
+  error(code: number, message: string, inserts?: Array<string>, numInserts?: Array<number>, timeInserts?: Array<string>, language?: string): void;
+  warn(code: number, message: string, inserts?: Array<string>, numInserts?: Array<number>, timeInserts?: Array<string>, language?: string): void;
+  log(code: number, message: string, inserts?: Array<string>, numInserts?: Array<number>, timeInserts?: Array<string>, language?: string): void;
+  info(code: number, message: string, inserts?: Array<string>, numInserts?: Array<number>, timeInserts?: Array<string>, language?: string): void;
+  debug(code: number, message: string, inserts?: Array<string>, numInserts?: Array<number>, timeInserts?: Array<string>, language?: string): void;
+  trace(code: number, message: string, inserts?: Array<string>, numInserts?: Array<number>, timeInserts?: Array<string>, language?: string): void;
+}
+
+type LogLevel = 'critical' | 'error' | 'warn' | 'log' | 'info'| 'debug' | 'trace'
